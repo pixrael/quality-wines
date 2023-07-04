3720e64db6184a218d44ad94cec3b67658af0da7
Set Israel Ortiz Monasterio <set.israel@globalsysinfo.com>
2023-07-04 14:41:01 +0200

    fix label on the select

--- client/quality-wines-web/src/components/select-qww/SelectQWW.tsx
+++ client/quality-wines-web/src/components/select-qww/SelectQWW.tsx
@@ -41,7 +41,10 @@ export default function SelectQWW<T extends FieldValues>({
     const inputLabelDefaultParams = {};//add here any default params
     const inputLabelParams = { ...inputLabelDefaultParams, ...selectParams?.inputLabel };
 
-    const selectDefaultParams = {};//add here any default params
+    const selectDefaultParams = {
+        label: label || capitalizeFirstLetter(registerName)
+
+    };//add here any default params
     const selectParamsCmp = { ...selectDefaultParams, ...selectParams?.select };
 
     const menuItemDefatultParams = {};//add here any default params
@@ -65,28 +68,14 @@ export default function SelectQWW<T extends FieldValues>({
     return (
         <Box sx={{ minWidth: 120 }} className="select-form" >
             <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Age</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    defaultValue={''}
-                    label="Age"
-                    onChange={() => { }}
-                >
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-            </FormControl>
-            <FormControl fullWidth>
-                <InputLabel {...inputLabelParams} id="demo-simple-select-label" >{label || capitalizeFirstLetter(registerName)}</InputLabel>
+                <InputLabel {...inputLabelParams} id="select-qww-label" >{label || capitalizeFirstLetter(registerName)}</InputLabel>
                 <Select
                     {...register(registerName, registerOptions)}
                     {...selectParamsCmp}
                     onChange={() => onChange()}
                     error={!!(errors && errors[registerName])}
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
+                    labelId="select-qww-label"
+                    id="select-qww"
                 >
                     {options.map(option => <MenuItem key={option.value} value={option.value} {...menuItemParams}>{option.label}</MenuItem>)}
 
