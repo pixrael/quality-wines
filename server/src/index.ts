6b7ba20a00cde77b607032baa0917ac7151fc013
Set Israel Ortiz Monasterio <set.israel@globalsysinfo.com>
2023-07-10 16:44:01 +0200

    allow all connections

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 
 app.use((req, res, next) => {
     // Set the appropriate headers for CORS
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials
