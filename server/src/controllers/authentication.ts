12824369ddf23eca70fe2788e9c0b713f8e68812
Set Israel Ortiz Monasterio <set.israel@globalsysinfo.com>
2023-07-11 10:59:50 +0200

    working with cookies

--- server/src/controllers/authentication.ts
+++ server/src/controllers/authentication.ts
@@ -29,7 +29,7 @@ export const login = async (req: express.Request, res: express.Response) => {
 
     await user.save();
 
-    res.cookie(COOKIES_AUTH, user.authentication.sessionToken, { domain: WHITELIST_IP_1, path: '/' });
+    res.cookie(COOKIES_AUTH, user.authentication.sessionToken/* , { domain: WHITELIST_IP_1, path: '/' } */);
 
     return res.status(200).json(user).end();
   } catch (error) {
